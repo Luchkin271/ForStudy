6ddd515cb081cf537edc882cbc3854a5b0b56b9d
Grigoriy_Luchkin <luchkin.grigoriy.a@gmail.com>
2025-06-28 00:35:45 +0300

    A simple tree has been created

---
+++ MyTree.cs
@@ -0,0 +1,20 @@
+﻿namespace ForStady
+{
+    internal class MyTree
+    {
+        public int Value { get; }
+        private List<MyTree> _children = new List<MyTree>();
+        public MyTree(int value, MyTree root)
+        {
+            Value = value;
+            root._children.Add(this);
+        }
+        public MyTree? FindExample(int value)
+        {
+            if (Value == value) return this;
+            MyTree ?findedMyTree = _children.FirstOrDefault(child => child.Value==value);
+            return findedMyTree;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+
+//Для Начала вспомним базовый материал на примере задачи: 
+//Создать класс реализующий простое дерево сфункциями добавления и пойска
